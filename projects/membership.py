219d2d8a0a2d579ea40911dba2d3acea43328c92
Ren Pang <ain-soph@live.com>
2021-10-30 17:46:20 -0400

    fix membership imports

--- projects/membership.py
+++ projects/membership.py
@@ -2,13 +2,13 @@
 
 # python projects/membership.py --model densenet121_comp --dataset cifar100 --pretrain
 
-from trojanzoo.utils.tensor import to_numpy
-import trojanvision.environ
-import trojanvision.datasets
-import trojanvision.models
-import trojanvision.trainer
-import trojanvision.attacks
-from trojanvision.utils import summary
+from autovul.base.utils.tensor import to_numpy
+import autovul.vision.environ
+import autovul.vision.datasets
+import autovul.vision.models
+import autovul.vision.trainer
+import autovul.vision.attacks
+from autovul.vision.utils import summary
 import argparse
 import warnings
 import time
@@ -16,13 +16,13 @@ warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    trojanvision.environ.add_argument(parser)
-    trojanvision.datasets.add_argument(parser)
-    trojanvision.models.add_argument(parser)
+    autovul.vision.environ.add_argument(parser)
+    autovul.vision.datasets.add_argument(parser)
+    autovul.vision.models.add_argument(parser)
     args = parser.parse_args()
-    env = trojanvision.environ.create(**args.__dict__)
-    dataset = trojanvision.datasets.create(**args.__dict__)
-    model = trojanvision.models.create(dataset=dataset, **args.__dict__)
+    env = autovul.vision.environ.create(**args.__dict__)
+    dataset = autovul.vision.datasets.create(**args.__dict__)
+    model = autovul.vision.models.create(dataset=dataset, **args.__dict__)
 
     if env['verbose']:
         summary(env=env, dataset=dataset, model=model)
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     import torch
     import numpy as np
     from sklearn import metrics
-    from trojanzoo.utils.data import dataset_to_list
+    from autovul.base.utils.data import dataset_to_list
     from art.estimators.classification import PyTorchClassifier  # type: ignore
     classifier = PyTorchClassifier(
         model=model._model,
@@ -58,14 +58,14 @@ if __name__ == '__main__':
     x_valid, y_valid = dataset_to_list(dataset.get_dataset('valid'))
     x_valid, y_valid = to_numpy(torch.stack(x_valid)), to_numpy(y_valid)
     # y_valid = np.argsort(classifier.predict(x_valid), axis=1)[:,-2]
-    
+
     # preds = np.argmax(classifier.predict(x_valid), axis=1)
     # v_idx = np.arange(len(x_valid))[preds == y_valid]
 
     t_idx = np.arange(len(x_train))
     v_idx = np.arange(len(x_valid))
     # print(t_idx.shape, v_idx.shape)
-    
+
     np.random.seed(int(time.time()))
     np.random.shuffle(t_idx)
     np.random.shuffle(v_idx)
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     hsj = HopSkipJump(classifier=classifier, targeted=False, norm=2, max_iter=max_iter, init_size=init_size, init_eval=init_eval, max_eval=max_eval, verbose=verbose)
     x_adv = hsj.generate(x=x, y=None)
     distance = np.linalg.norm((x_adv - x).reshape((x.shape[0], -1)), ord=np.inf, axis=1)
-    
+
     # print(distance)
     # print(y_truth)
 
