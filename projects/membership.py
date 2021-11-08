b070d349fba6c127ac1920cafa6848cd4fc89653
Ren Pang <ain-soph@live.com>
2021-11-08 13:56:50 -0500

    fix membership

--- projects/membership.py
+++ projects/membership.py
@@ -2,14 +2,10 @@
 
 # python scripts/membership.py --model densenet121_comp --dataset cifar100 --pretrain
 
-from trojanzoo.utils.tensor import to_numpy
-import trojanvision.environ
-import trojanvision.datasets
-import trojanvision.models
-import trojanvision.trainer
-import trojanvision.attacks
+import autovul.vision
+from autovul.vision import to_numpy
+
 from scipy.special import softmax
-# from trojanvision.utils import summary
 import argparse
 import warnings
 import numpy as np
@@ -19,13 +15,13 @@ warnings.filterwarnings("ignore")
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    trojanvision.environ.add_argument(parser)
-    trojanvision.datasets.add_argument(parser)
-    trojanvision.models.add_argument(parser)
+    autovul.vision.environ.add_argument(parser)
+    autovul.vision.datasets.add_argument(parser)
+    autovul.vision.models.add_argument(parser)
     args = parser.parse_args()
-    env = trojanvision.environ.create(**args.__dict__)
-    dataset = trojanvision.datasets.create(**args.__dict__)
-    model = trojanvision.models.create(dataset=dataset, **args.__dict__)
+    env = autovul.vision.environ.create(**args.__dict__)
+    dataset = autovul.vision.datasets.create(**args.__dict__)
+    model = autovul.vision.models.create(dataset=dataset, **args.__dict__)
 
     import torch
     from sklearn import metrics
